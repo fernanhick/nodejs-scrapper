cf7e18dc71f203f0b3805b7f73079f4fac6bedac
Fernando Hickman <fernanhick@gmail.com>
2022-04-28 19:11:28 +0100

    price detect sales item

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const { response } = require('express')
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { response } = require("express");
+const ObjectsToCsv = require("objects-to-csv");
 
-const PORT = 8000
+const PORT = 8000;
+let itemCount = 0;
+const app = express();
 
+const url = "https://www.size.co.uk/mens/footwear/brand/nike/?max=204&AJAX=1";
 
-const app = express()
-
-
-const url = 'https://www.size.co.uk/mens/footwear/'
+fs = require("fs");
 
 // Call the axios library to the speficyc website
 axios(url)
-// get the response into the result parameter
-.then((result) => {
-    // store the data of the result into the html variable
-    const html = result.data
-    // call the cheerio library to load the data stored into the html variable
-    // this is done by calling the load method and passing the data stored in html variable 
-    // store this into the $ variable
-    const  $ = cheerio.load(html)
+    // get the response into the result parameter
+    .then((result) => {
+        // store the data of the result into the html variable
+        const html = result.data;
 
-    // create an array to store each item found
-    const items = []
+        // call the cheerio library to load the data stored into the html variable
+        // this is done by calling the load method and passing the data stored in html variable
+        // store this into the $ variable
+        const $ = cheerio.load(html);
 
-    // For each item found with the class name 'itemContainer' we will get the text included
-    $('.itemInformation', html).each(function () {
-                
-/*         const name = $('.itemTitle').text()
+        // create an array to store each item found
+        const items = [];
+
+        // For each item found with the class name 'itemContainer' we will get the text included
+        $(".itemInformation", html).each(function () {
+            /*         const name = $('.itemTitle').text()
         const price = $('.itemPrice').text()
 
-       items.push({
-           name,
-           price
-       }) */ 
+       */
 
-        //      TO-DO Isolate each of the variable into an array
+            //      TO-DO Isolate each of the variable into an array
+            itemCount++;
+            const title = $(this).find("a").text(); // 51%
+            let price = $(this).find(".pri").text();
+            if (price.length > 8) {
+                price = `${price.slice(6, 14).trim(" ")}`;
+            }
+            // console.log(`Name: ${title} price: ${price}`);
+            items.push({
+                title,
+                price,
+            });
+        });
 
-       const a = $(this).text(); // 51%
-       const item = a.slice(' ')
-       console.log(a)
-      
+        //Store array into csv file
+        const csv = new ObjectsToCsv(items);
+        async function writeToFile(items) {
+            await csv.toDisk("./secondary-file.csv");
+        }
+        writeToFile();
+
+        console.log(items);
+        console.log(items.length);
     })
-
-    const checkChanges = async (items) =>{
-        // Compare each items if the price have been changes
-    }
-
-    
-    
-}).catch((err) => {
-    console.log('Error found', err)
-});
-
+    .catch((err) => {
+        console.log("Error found", err);
+    });
 
 // Express listen to port assigned
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
-
-
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
