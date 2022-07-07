68a7ebd86c68e17d4b6348cac2aee6dcd8663e47
Fernando Hickman <fernanhick@gmail.com>
2022-07-07 23:20:24 +0100

    json file creation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const { response } = require("express");
-const ObjectsToCsv = require("objects-to-csv");
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { response } = require('express')
+const ObjectsToCsv = require('objects-to-csv')
 // const { generateDeltaFile } = require("./csvcomparison");
-const PORT = 8000;
-const app = express();
+const PORT = 8000
+const app = express()
 
-const url = "https://www.size.co.uk/mens/footwear/brand/nike/?max=204&AJAX=1";
+const url = 'https://www.size.co.uk/mens/footwear/brand/nike/?max=204&AJAX=1'
 
-fs = require("fs");
+fs = require('fs')
 
 // Call the axios library to the speficyc website
 const getData = () => {
@@ -17,50 +17,67 @@ const getData = () => {
         // get the response into the result parameter
         .then((result) => {
             // store the data of the result into the html variable
-            const html = result.data;
+            const html = result.data
 
             // call the cheerio library to load the data stored into the html variable
             // this is done by calling the load method and passing the data stored in html variable
             // store this into the $ variable
-            const $ = cheerio.load(html);
+            const $ = cheerio.load(html)
 
             // create an array to store each item found
-            const items = [];
+            const items = []
+            const product = []
 
             // For each item found with the class name 'itemContainer' we will get the text included
-            $(".itemInformation", html).each(function () {
+            $('.itemContainer', html).each(function () {
                 /*         const name = $('.itemTitle').text()
         const price = $('.itemPrice').text()
 
        */
 
                 //      TO-DO Isolate each of the variable into an array
-                const title = $(this).find("a").text(); // 51%
-                let price = $(this).find(".pri").text();
+                const title = $(this).find('.itemInformation').find('a').text() // 51%
+                let price = $(this).find('.itemInformation').find('.pri').text()
+
+                const img = $(this).find('.img-placeholder').attr('data-src')
+                console.log(img)
                 if (price.length > 8) {
-                    price = `${price.slice(6, 14).trim(" ")}`;
+                    price = `${price.slice(6, 14).trim(' ')}`
                 }
                 // console.log(`Name: ${title} price: ${price}`);
                 items.push({
                     title,
                     price,
-                });
-            });
+                    img,
+                })
+                product.push({
+                    name: title,
+                    price: price,
+                    img: img,
+                })
+            })
 
+            var json = JSON.stringify(product)
+
+            var fs = require('fs')
+
+            fs.writeFile('myjsonfile.json', json, function (err, result) {
+                if (err) console.log('error', err)
+            })
             //Store array into csv file
-            const csv = new ObjectsToCsv(items);
+            const csv = new ObjectsToCsv(items)
             async function writeToFile(items) {
-                await csv.toDisk("./secondary-file.csv");
+                await csv.toDisk('./secondary-file.csv')
             }
-            writeToFile();
+            writeToFile()
 
             //   console.log(items);
-            console.log(items.length);
+            console.log(items.length)
         })
         .catch((err) => {
-            console.log("Error found", err);
-        });
-};
-setInterval(getData, 10000);
+            console.log('Error found', err)
+        })
+}
+setInterval(getData, 10000)
 // Express listen to port assigned
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
